588a8e8ddf4245bc8be0d25ba3161e1d27c39e23
Cory Tamburrino <ctamburrino@sandiego.edu>
2025-04-03 13:27:20 -0700

    Updated Main to call welcomeToHopfield

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 /*
- * This program is the main driver of the percetpton neural network.  The program
- * reads implements a UI to get user input, loads stored information from training 
+ * This program is the main driver of the discrete hopfield neural network.  The program
+ * implements a UI to get user input, loads stored information from training 
  * data files, creates and trains a neural network by modifying and saving weight values.
  * 
- * It also tests new datasets using saved trained weight files to classify the new data
+ * It also tests new datasets using saved trained weight files to associate the new data
  * samples based on the training data it was fed.
  * 
  * Authors:
@@ -11,11 +11,11 @@
  * - David Kujawinski
  * - Dinh Troung
  * 
- * Date Last Modified: 3/7/2025
+ * Date Last Modified: 4/3/2025
  */
 
 public class Main {
     public static void main(String[] args){
-        UserIO.welcomeToPerceptron();
+        UserIO.welcomeToHopfield();
     }
 }
