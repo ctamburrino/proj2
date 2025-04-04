35fc7b54e58270b74f29c8cefe3c2472abfd997a
Cory Tamburrino <ctamburrino@sandiego.edu>
2025-04-03 22:21:53 -0700

    Added test method implementation.  Has not been tested yet.

--- NeuralNet.java
+++ NeuralNet.java
@@ -14,10 +14,10 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
-import java.util.Random;
-import java.lang.StringBuilder;
 
 public class NeuralNet {
     public static boolean train(TrainingSettings netTrainingSettings){
@@ -92,163 +92,120 @@ public class NeuralNet {
             e.printStackTrace();
         }
     }
-}
 
-//     public static void test(TestingSettings netTestingSettings){
-//     /*
-//     Tests neural net with dataset and trained weights.
+    public static int[][] test(TestingSettings netTestingSettings){
+    /*
+    Tests neural net with dataset and trained weights.
 
-//     Parameters:
-//     -Testing SettingsnetTestingSettings: Data structure that holds testing information provided by user.
-//     */
-//         // Load trained weight matrices from file
-//         int [][] trainedWeightMatrix = netTestingSettings.trainedWeightMatrix;
+    Parameters:
+    -Testing Settings netTestingSettings: Data structure that holds testing information provided by user.
+    */
+        // Load trained weight matrices from file
+        int [][] trainedWeightMatrix = netTestingSettings.trainedWeightMatrix;
+        int numNodes = netTestingSettings.numNodes;
 
-//         // Get dataset to test
-//         List<DataSample> dataset = netTestingSettings.dataset;
+        // Get dataset to test
+        List<DataSample> dataset = netTestingSettings.dataset;
+        int[][] netClassifications = new int[dataset.size()][numNodes];
 
-//         // Create net architecture from first data sample in dataset
-//         DataSample firstSample = dataset.get(0);
-//         int numOutputNodes = firstSample.getOutputDimension();
-//         int numSamples = dataset.size();
-//         List<int> randomSample = new List<int>[numOutputNodes];
-//         for(int i = 0; i<numOutputNodes; i++){
-//             randomSample[i] = i;
-//         }
-//         Random random = new Random();
+        int sampleNum = 0;
+        for (DataSample sample : dataset){
+            sampleNum++;
+            boolean converged = false;
+            int[] xArray = sample.getPixelArray();
+            int[] yArray = Arrays.copyOf(xArray, xArray.length);
+            List<Integer> nodes = new ArrayList<>(numNodes);
+            for (int i = 0; i < numNodes; i++){
+                nodes.add(i);
+            }
+            Collections.shuffle(nodes);
+            while(!converged){
+                yArray = Arrays.copyOf(xArray, xArray.length);
+                boolean activationChanged = false;
+                for (int i = 0; i < numNodes; i++){
+                    int yIn = calculateYIn(trainedWeightMatrix, xArray, i, yArray);
+                    int yOut = applyActivationFunction(yIn, yArray[i]);
+                        if (yArray[i] != yOut){
+                            yArray[i] = yOut;
+                            activationChanged = true;
+                        }
+                    }
+                if (activationChanged == false){
+                    converged = true;
+                }else{
+                    xArray = Arrays.copyOf(yArray, yArray.length);;
+                }
+            }
+            netClassifications[sampleNum] = yArray;
+        }
 
-//         // Deploy Neural Net
-//         int[][] netClassifications = new int[numSamples][numOutputNodes];
-//         for(int sampleNum = 0; sampleNum < dataset.size(); sampleNum++){
-//             int[] yIn = new int[numOutputNodes];
-//             DataSample sample = dataset.get(sampleNum);
-//             int[] inputSignals = sample.getPixelArray();
-//             int[] yOut = inputSignals;
-//             for (int outputNode = 0; outputNode < numOutputNodes; outputNode++) {        
-//                 int randomIndex = randomSample.get(random.nextInt(randomSample.size));
-//                 yIn[randomIndex] = calculateYIn(trainedWeightMatrix, inputSignals, randomIndex, yOut);
-//                 yOut[outputNode] = applyActivationFunction(yIn[randomIndex], yOut[randomIndex]);
-//             }
-//             netClassifications[sampleNum] = yOut;
-//         }
+        // // Create net architecture from first data sample in dataset
+        // DataSample firstSample = dataset.get(0);
+        // int numOutputNodes = firstSample.getOutputDimension();
+        // int numSamples = dataset.size();
+        // List<int> randomSample = new List<int>[numOutputNodes];
+        // for(int i = 0; i<numOutputNodes; i++){
+        //     randomSample[i] = i;
+        // }
+        // Random random = new Random();
 
+        // // Deploy Neural Net
 
+        // for(int sampleNum = 0; sampleNum < dataset.size(); sampleNum++){
+        //     int[] yIn = new int[numOutputNodes];
+        //     DataSample sample = dataset.get(sampleNum);
+        //     int[] inputSignals = sample.getPixelArray();
+        //     int[] yOut = inputSignals;
+        //     for (int outputNode = 0; outputNode < numOutputNodes; outputNode++) {        
+        //         int randomIndex = randomSample.get(random.nextInt(randomSample.size));
+        //         yIn[randomIndex] = calculateYIn(trainedWeightMatrix, inputSignals, randomIndex, yOut);
+        //         yOut[outputNode] = applyActivationFunction(yIn[randomIndex], yOut[randomIndex]);
+        //     }
+        //     netClassifications[sampleNum] = yOut;
+        // }
+
+        return netClassifications;
         
-//         saveResultsToFile(netClassifications, netTestingSettings.testingResultsOutputFilePath);
-//     }
+        //saveResultsToFile(netClassifications, netTestingSettings.testingResultsOutputFilePath);
+    }
 
-//     public static int calculateYIn(int[][] weightMatrix, int[] inputSignals, int index, int[] yOut) {
-//         /*
-//         This method calculates the y in value for the corresponding pattern.
+    public static int calculateYIn(int[][] weightMatrix, int[] xArray, int neuronNum, int[] yArray) {
+        /*
+        This method calculates the y in value for the corresponding pattern.
     
-//         Parameters:
-//         - int[][] weightMatrix: Matrix of current weight values
-//         - int[] biasWeights: Array of current bias weight values
-//         - int[] inputSignals: pixels of the current sample
-//         - int outputNode: current outputNode being trained for
+        Parameters:
+        - int[][] weightMatrix: Matrix of current weight values
+        - int[] biasWeights: Array of current bias weight values
+        - int[] inputSignals: pixels of the current sample
+        - int outputNode: current outputNode being trained for
     
-//         Return:
-//         - int representing computed YIn
-//         */
-//             int computedYIn = inputSignals[index];
-//             for (int i = 0; i < inputSignals.length; i++) {
-//                 computedYIn += yOut[i] * weightMatrix[i][index];
-//             }
-//             return computedYIn;
-//         }
+        Return:
+        - int representing computed YIn
+        */
+            int computedYIn = xArray[neuronNum];
+            for (int yElement = 0; yElement < yArray.length; yElement++) {
+                computedYIn += yArray[yElement] * weightMatrix[yElement][neuronNum];
+            }
+            return computedYIn;
+        }
     
     
-//         public static int applyActivationFunction(int yIn, int yOut) {
-//         /*
-//         Applies activation function to value
+        public static int applyActivationFunction(int yIn, int prevY) {
+        /*
+        Applies activation function to value.
     
-//         Parameters:
-//         - int yIn: value to be apply activation function on
+        Parameters:
+        - int yIn: value to be apply activation function on
     
-//         Return:
-//         int representing output of function
-//         */
-//             if (yIn > 0) {
-//                 return 1;
-//             } else if(yIn < 0){
-//                 return -1;
-//             } else {
-//                 return yOut;
-//             }
-//         }
-
-
-//         /*
-//         * a method that turns an array into a string without brackets or commas, with each element being space separated
-//         *
-//         * Parameters:
-//         * int[] array - the array to be transformed
-//         *
-//         * Returns:
-//         * a String of the transformed array
-//         *
-//         * */
-//         public static String arrayToString(int[] array){
-//             StringBuilder sb = new StringBuilder();
-//             for(int i=0; i<array.length; i++){
-//                 sb.append(array[i]+" ");
-//             }
-//             return sb.toString();
-//         }
-
-//         /*Override the toString such that it prints out in the required format
-//         *
-//         * Parameters:
-//         * None
-//         *
-//         * Returns:
-//         * a String of the name followed by a new line and the array transformed by the arrayToString() method
-//         *
-//         * */
-//         @Override
-//         public String toString(){
-//             return this.name()+"\n"+Label.arrayToString(this.output);
-//         }
-//     }
-
-//     public static void saveResultsToFile(int[][] classifications, String testingResultsOutputFilePath){
-//     /*
-//     Saves classification results from testing to output file specified by user
-
-//     Parameters:
-//     - int[][] classifications: Matrix representing classification results
-//     - String testingResultsOutputFilePath: filepath of output file specified by user
-//     */
-//         try (BufferedWriter writer = new BufferedWriter(new FileWriter(testingResultsOutputFilePath))) {
-//             Label[] actualOutput = Label.values();
-//             int labelIncrement = 0;
-//             for (int[] row : classifications){
-//                 //identifies the label it was trying to classify
-//                 Label classifiedLabel = Label.getLabel(row);
-//                 //prints out the expected result
-//                 writer.write("Actual:\n"+actualOutput[labelIncrement].toString());
-//                 writer.newLine();
-//                 //prints out what it was classified as, and Undecided if the vector was not identified
-//                 if(classifiedLabel==null){
-//                     writer.write("Classified:\nUndecided\n"+Label.arrayToString(row));
-//                 }
-//                 else{
-//                     writer.write("Classified:\n"+classifiedLabel.toString());
-//                 }
-//                 //increments the labels
-//                 if(labelIncrement==6)
-//                     labelIncrement = 0;
-//                 else
-//                     labelIncrement++;
-//                 writer.newLine();
-//                 writer.newLine();
-//             }
-//             writer.newLine();
-//             writer.newLine();
-//             writer.close();
-//             System.out.println("Results saved successfully to " + testingResultsOutputFilePath + "\n");
-//         } catch (IOException e){
-//             e.printStackTrace();
-//         }
-//     }
-// }
+        Return:
+        int representing output of function
+        */
+            if (yIn > 0) {
+                return 1;
+            } else if(yIn < 0){
+                return -1;
+            } else{
+                return prevY;
+            }
+        }
+}
